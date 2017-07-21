81c6aabe687be83ab28650c1ce035f9ea07b7a83
musikzauberin <jialelim@hotmail.com>
2017-07-21 21:02:21 +0800

    PoundHill

--- newCode/PoundHillData.R
+++ newCode/PoundHillData.R
@@ -65,4 +65,43 @@ par(mar=c(12,5,1,1))
 plot(Count~Species, data = MyWrangledData, las = 2, xlab = "") # remove x axis label
 mtext("Species", side=1, line=10, las = 1) # add manually
 
+# reorganising data
+library(reshape2)
+library(plyr)
+# Calculating total number of individuals per plot
+totalcount <- tapply(MyWrangledData$Count, list(MyWrangledData$Plot, MyWrangledData$Species), sum, na.rm = TRUE)
+totalcount
+
+# Calculate number of quadrats per plot and store as column in totalcount
+quadno <- ddply(MyWrangledData, c("Plot"), summarise, Quadrat.number = length(unique(Quadrat)))
+DF <- cbind(totalcount, quadno$Quadrat.number) # combine columns of totalcount and quadrat.no 
+colnames(DF)[ncol(DF)] <- "Quadrat.no" # add column name to last column of DF.meancount
+
+DF
+
+# Calculate mean number of counts of each species per quadrat in each plot
+# divide all columns except the last column by the last column (no.of quadrat per plot)
+# multiply by 8 to calculate counts / m^2 for each species for each plot
+DF.meancount <- DF[, 1:(ncol(DF)-1)] / DF[, ncol(DF)] *8 
+DF.meancount
+
+# rearrange into Plot Species and Count variables
+DF.Count <- melt(DF.meancount, id=c("Plot", "Species"), 
+           varnames = c("Plot", "Species") , value.name = "Count")
+head(DF.Count)
+
+# reorder using plot number
+PlotData <- DF.Count[order(DF.Count$Plot),]
+
+head(PlotData)
+
+# replace Count with presence/absence data # PlotData[,3] 3rd column of PlotData
+PlotData[PlotData[,3] > 0, 3] <- 1 
+
+SpeciesPerPlot <- tapply(PlotData$Count, list(PlotData$Plot), sum, na.rm = TRUE)
+
+SpeciesPerPlot
+
+# Plotting
+plot(SpeciesPerPlot)
 
